1a66d24507ef5c8c6de843b4dcab842f69f95b8a
pkmitl205 <58030205@kmitl.ac.th>
2017-10-23 21:48:06 +0700

    การทดลอง การใช้งานคำสั่ง break ร่วมกับคำสั่งวนรอบ (for)

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace Lab04
+namespace ConsoleApp2
 {
-    class Program
+    class BreakTest
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
 
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
