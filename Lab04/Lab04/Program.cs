50a8a4c85d0d605492d4728cd00d37f1bdf67824
pkmitl205 <58030205@kmitl.ac.th>
2017-10-23 22:48:22 +0700

    การทดลอง การใช้งานคำสั่ง continue

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace Lab04
+namespace ConsoleApp2
 {
-    class Program
+    class ContinueTest 
     {
-        static void Main(string[] args)
+        static void Main()
         {
-
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i < 9)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
